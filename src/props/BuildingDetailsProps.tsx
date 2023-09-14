926759ff8d3a51ef550f7867307fba14d843cf3e
Esparguetes <diogobarrosmartins@hotmail.com>
2023-09-14 23:46:59 +0100

    fix building component

--- src/props/BuildingDetailsProps.tsx
+++ src/props/BuildingDetailsProps.tsx
@@ -34,6 +34,23 @@ endTime: number;
 villageId: number;
 };
 
+
+
+const buildingDescriptions: { [key: string]: string } = {
+  'FARM': 'A place to cultivate crops.',
+  'QUARRY': 'Extracts stone from the earth.',
+  'MINE': 'A deep pit where minerals are extracted.',
+  'FOREST': 'A dense collection of trees.',
+  'PUB': 'A place for villagers to relax.',
+  'BARRACKS': 'Trains and houses soldiers.',
+  'GRAIN_SILO': 'Stores grains for future use.',
+  'STORAGE': 'A place to store goods.',
+  'RESEARCH_CENTER': 'Innovates and discovers new techniques.',
+  'STABLE': 'Houses and trains cavalry units.',
+  'SIEGE_WORKSHOP': 'Produces siege weapons.'
+  };
+  
+
 const BuildingDetails: React.FC<BuildingDetailsProps> = ({
 building,
 onClose,
@@ -62,21 +79,6 @@ useEffect(() => {
     fetchTroops();
   }, [building.id]);
 
-
-
-const buildingDescriptions: { [key: string]: string } = {
-'FARM': 'A place to cultivate crops.',
-'QUARRY': 'Extracts stone from the earth.',
-'MINE': 'A deep pit where minerals are extracted.',
-'FOREST': 'A dense collection of trees.',
-'PUB': 'A place for villagers to relax.',
-'BARRACKS': 'Trains and houses soldiers.',
-'GRAIN_SILO': 'Stores grains for future use.',
-'STORAGE': 'A place to store goods.',
-'RESEARCH_CENTER': 'Innovates and discovers new techniques.',
-'STABLE': 'Houses and trains cavalry units.',
-'SIEGE_WORKSHOP': 'Produces siege weapons.'
-};
 
 
 const isBuildingUpgrading = (): boolean => {
@@ -118,18 +120,9 @@ try {
 
 
 return (
-  <div
-    style={{
-      zIndex: 10,
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      backgroundColor: 'white',
-      padding: '16px',
-      border: '1px solid black',
-    }}
-  >
+
+  <div className="buildingDetailsWrapper">
+  
     <h4>{building.type}</h4>
     <p>{buildingDescriptions[building.type]}</p>
 
