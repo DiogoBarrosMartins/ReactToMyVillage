1a484d1a73f0f4841a9e70628f492535c352791b
Esparguetes <diogobarrosmartins@hotmail.com>
2023-09-29 23:49:31 +0100

    Update TroopInfo.tsx

--- src/props/TroopInfo.tsx
+++ src/props/TroopInfo.tsx
@@ -34,7 +34,6 @@ const TroopInfo: React.FC<TroopInfoProps> = ({ building }) => {
       try {
         const data = await fetchAvailableTroops(building.id);
         setTroopTypes(data);
-        // Automatically selects the first troop type
         setSelectedTroop(data[0]?.name || null); 
       } catch (error) {
         console.error('Failed to fetch troops:', error);
